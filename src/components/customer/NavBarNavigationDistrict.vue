1d020c45a754a333abba134eae1b9a76a9865a16
fefefafa01 <khoa.trinhkaiz@hcmut.edu.vn>
2024-06-07 16:35:42 +0700

    create location

--- src/components/customer/NavBarNavigationDistrict.vue
+++ src/components/customer/NavBarNavigationDistrict.vue
@@ -1,49 +1,68 @@
 <script setup>
 import { ref, onMounted } from 'vue';
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiMapMarker } from '@mdi/js';
 
 const district = ref(['Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6']);
 const selectedDistrict = ref('');
-const districtTemp = ref([]);
+const districtTemp = ref(district.value);
 const isMenuVisible = ref(false);
+const path = ref(mdiMapMarker);
 
 onMounted(() => {
-  selectedDistrict.value = localStorage.getItem('selectedDistrict');
-  districtTemp.value = district.value;
+  const storedDistrict = localStorage.getItem('selectedDistrict');
+  if (storedDistrict) {
+    selectedDistrict.value = storedDistrict;
+  }
 });
+
+const togglePlaceMenu = () => {
+  isMenuVisible.value = !isMenuVisible.value;
+};
+
+const handleKeyUp = (event) => {
+  const searchedVal = event.target.value.toLowerCase();
+  districtTemp.value = district.value.filter((data) => data.toLowerCase().includes(searchedVal));
+};
+
+const updateName = (selectedLi) => {
+  selectedDistrict.value = selectedLi;
+  localStorage.setItem('selectedDistrict', selectedLi);
+  isMenuVisible.value = false;
+  // Add redirect logic here if needed
+};
 </script>
 
 <template>
-  <div class="text-white h-14 relative w-40 max-w-40 flex items-center">
+  <div>
     <div
-      class="duration-[400] round-2xl hover:bg-customBgHover hover:text-customTextHover"
-      type="button"
+      class="relative duration-[400ms] hover:text-customTextHover cursor-pointer flex items-center"
+      @click="togglePlaceMenu"
     >
-      <img
-        class="top-[16px] left-0 w-[30px] h-[30px]"
-        alt="Icon location"
-        src="https://c.animaapp.com/UGutMkT8/img/---icon--location--1@2x.png"
-      />
-      <div
-        class="absolute top-1/2 transform -translate-y-1/2 font-bold font-comfortaa text-lg ml-[40px] whitespace-normal select-btn"
-      >
-        {{ selectedDistrict || 'Địa điểm' }}
+      <svg-icon type="mdi" :path="path" class="" alt="Icon location" />
+      <div class="font-bold text-lg ml-2">
+        <p>{{ selectedDistrict || 'Địa điểm' }}</p>
       </div>
     </div>
+
     <div
-      class="hidden w-[160%] px-20 mt-80 rounded-lg bg-white text-[#0b0c10]"
-      :class="{ block: isMenuVisible }"
+      class="absolute p-5 rounded-lg bg-white text-[#0b0c10] shadow-lg"
+      :class="isMenuVisible ? 'block' : 'hidden'"
     >
-      <div class="relative flex">
-        <span
-          class="text-[#999] m-13 absolute inline-block w-[1.5em] h-[1.5em] bg-no-repeat bg-contain bg-black"
-        ></span>
-        <input type="text" placeholder="Tìm kiếm" @keyup="handleKeyUp" />
+      <div class="relative flex mb-2">
+        <span class="uil--search-alt"></span>
+        <input
+          class="w-full h-12 text-lg pl-10 border rounded-md"
+          type="text"
+          placeholder="Tìm kiếm"
+          @keyup="handleKeyUp"
+        />
       </div>
-      <ul class="mt-10 pl-0 max-h-60 overflow-y-auto options">
+      <ul class="options max-h-[250px] overflow-y-auto">
         <li
-          class="text-base h-12 rounded-md flex items-center cursor-pointer"
           v-for="(dis, index) in districtTemp"
           :key="index"
+          class="text-[21px] h-12 rounded-md flex items-center px-2 cursor-pointer hover:bg-gray-200"
           @click="updateName(dis)"
         >
           {{ dis }}
@@ -54,6 +73,18 @@ onMounted(() => {
 </template>
 
 <style>
+.uil--search-alt {
+  color: #999;
+  margin: 13px;
+  position: absolute;
+  display: inline-block;
+  width: 1.5em;
+  height: 1.5em;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath fill='black' d='M21.07 16.83L19 14.71a3.08 3.08 0 0 0-3.4-.57l-.9-.9a7 7 0 1 0-1.41 1.41l.89.89a3 3 0 0 0 .53 3.46l2.12 2.12a3 3 0 0 0 4.24 0a3 3 0 0 0 0-4.29m-8.48-4.24a5 5 0 1 1 0-7.08a5 5 0 0 1 0 7.08m7.07 7.07a1 1 0 0 1-1.42 0l-2.12-2.12a1 1 0 0 1 0-1.42a1 1 0 0 1 1.42 0l2.12 2.12a1 1 0 0 1 0 1.42'/%3E%3C/svg%3E");
+}
+
 .options::-webkit-scrollbar {
   width: 7px;
 }
