5feda233ee94bd21282ca9c4f35bbb04cd19d2cc
fefefafa01 <khoa.trinhkaiz@hcmut.edu.vn>
2024-06-09 13:19:36 +0700

    new position

--- src/views/customer/HomeView.vue
+++ src/views/customer/HomeView.vue
@@ -6,10 +6,6 @@ import NavBar from '@/components/customer/NavBar.vue';
 
 const backend = import.meta.env.VITE_SERVER;
 
-const options = {
-  rewind: true
-};
-
 const notifData = ref([]);
 console.log(backend);
 onMounted(async () => {
@@ -28,11 +24,11 @@ onMounted(async () => {
     <NavBar />
     <div class="w-full h-auto mt-[115px] pt-[50px] pb-[20px]">
       <div class="w-full h-[500px]">
-        <Splide :options="{ options }" aria-label="My Favorite Images">
+        <Splide :options="{ rewind: true }" aria-label="My Favorite Images">
           <SplideSlide v-for="data in notifData" :key="data.notification_id">
             <img
               class="h-[500px] object-fill block w-full"
-              :src="data.link_img"
+              :src="`src/images/notification/${data.link_img}`"
               :alt="`Sample ${data.notification_id}`"
             />
           </SplideSlide>
