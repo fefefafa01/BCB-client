dba8e47ed619f5c62a81e5b9131d99fc6520f413
fefefafa01 <khoa.trinhkaiz@hcmut.edu.vn>
2024-06-09 11:24:57 +0700

    create notification

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import { createApp } from 'vue'
-import '@/css/style.css'
-import App from '@/App.vue'
-import Router from '@/router/index.js'
+import { createApp } from 'vue';
+import '@/css/style.css';
+import App from '@/App.vue';
+import Router from '@/router/index.js';
 
-const app = createApp(App)
-app.use(Router).mount('#app')
+import VueSplide from '@splidejs/vue-splide';
+import '@splidejs/vue-splide/css';
+
+const app = createApp(App);
+app.use(VueSplide).use(Router).mount('#app');
--- src/views/customer/HomeView.vue
+++ src/views/customer/HomeView.vue
@@ -1,11 +1,43 @@
 <script setup>
-import NavBar from '@/components/customer/NavBar.vue'
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import { Splide, SplideSlide } from '@splidejs/vue-splide';
+import NavBar from '@/components/customer/NavBar.vue';
+
+const backend = import.meta.env.VITE_SERVER;
+
+const options = {
+  rewind: true
+};
+
+const notifData = ref([]);
+console.log(backend);
+onMounted(async () => {
+  try {
+    const notification = await axios.get(backend + 'home/notif');
+    notifData.value = notification.data.notifData;
+    console.log(notification);
+  } catch (error) {
+    console.error(error);
+  }
+});
 </script>
 
 <template>
   <div class="back_ground">
     <NavBar />
+    <div class="w-full h-auto mt-[115px] pt-[50px] pb-[20px]">
+      <div class="w-full h-[500px]">
+        <Splide :options="{ options }" aria-label="My Favorite Images">
+          <SplideSlide v-for="data in notifData" :key="data.notification_id">
+            <img
+              class="h-[500px] object-fill block w-full"
+              :src="data.link_img"
+              :alt="`Sample ${data.notification_id}`"
+            />
+          </SplideSlide>
+        </Splide>
+      </div>
+    </div>
   </div>
 </template>
-
-<style lang="scss" scoped></style>
